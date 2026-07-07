9b9d9df70566e1f0d1dc62d4d89bb396ce419cab
Dev_fiorella <fiorellakat19@gmail.com>
2026-07-06 21:43:55 -0500

    ci: Agregar variables de entornog

--- test.sh
+++ test.sh
@@ -8,6 +8,10 @@ echo "=========================================="
 # Verificar versión de bash
 echo "✅ Bash version: $BASH_VERSION"
 
+# Imprimir el entorno de la variable configurada en GitHub Actions
+# Colocado aquí arriba para que se ejecute antes del exit
+echo "🌍 Entorno actual: $APP_ENV"
+echo "=========================================="
 # Verificar que un archivo existe
 if [ -f "README.md" ]; then
   echo "✅ README.md encontrado"
